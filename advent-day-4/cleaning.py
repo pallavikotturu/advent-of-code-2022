c5f118b6c88aafe4b1ba9cc8f3751906774d15ac
pkotturu3 <pkotturu@gatech.edu>
2022-12-13 22:07:57 -0500

    day 4 part one done

---
+++ advent-day-4/cleaning.py
@@ -0,0 +1,23 @@
+# if the digits that fall within the second half are encompassed within the first half, then increment containment counter
+# inputFile = open('cleaning-input-pallavi.txt', 'r')
+inputFile = open('cleaning-input-jeff.txt', 'r')
+inputFileLines = inputFile.readlines()
+
+def contains(lower1, upper1, lower2, upper2):
+    # if 3rd number >= first number and 4th number <= second number then increment containsFully
+    if lower2 >= lower1 and upper2 <= upper1 :
+        return True
+
+
+containsFully = 0
+
+# break down each line using the delimiter (,) into two sections
+for eachLine in inputFileLines:
+    (firstHalf, secondHalf) = eachLine.strip().split(",")
+    (lower1, upper1, lower2, upper2) = list(map(int, firstHalf.split('-') + secondHalf.split('-')))
+
+    if contains(lower1, upper1, lower2, upper2) or contains(lower2, upper2, lower1, upper1) :
+        # print(eachLine)
+        containsFully += 1
+
+print(containsFully)
