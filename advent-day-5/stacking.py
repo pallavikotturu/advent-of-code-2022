8a203bca71a5689cc8c1127d16ca40e622f0b2bc
jffhff <jffhff+github@thehoffmanns.net>
2022-12-14 20:53:35 -0500

    day 5, challenge 1

---
+++ advent-day-5/stacking.py
@@ -0,0 +1,51 @@
+# DAY 5
+# Python program to
+# follow list of instructions and move crates across 9 different stacks
+# for elves to work on
+
+inputFile = open('instructions-jeff.txt', 'r')
+inputFileLines = inputFile.readlines()
+
+jeffStacks = [
+    ['D', 'L', 'V', 'T', 'M', 'H', 'F'],
+    ['H', 'Q', 'G', 'J', 'C', 'T', 'N', 'P'],
+    ['R', 'S', 'D', 'M', 'P', 'H'],
+    ['L', 'B', 'V', 'F'],
+    ['N', 'H', 'G', 'L', 'Q'],
+    ['W', 'B', 'D', 'G', 'R', 'M', 'P'],
+    ['G', 'M', 'N', 'R', 'C', 'H', 'L', 'Q'],
+    ['C', 'L', 'W'],
+    ['R', 'D', 'L', 'Q', 'J', 'Z', 'M', 'T']
+]
+
+stacks = jeffStacks
+
+# split each line using delimiter ' '
+# ignore all words.
+# capture the 3 numbers.
+# convert them into integers
+# create function moveCrates (first number is quantity, second number is stack start location, third number is stack end location)
+# within the function, we pop and then append based on location values -1 from stacks
+
+def moveCrates(quantity, startStack, finalStack):
+    times = 1
+    while times <= quantity:
+        crateToMove = stacks[startStack-1].pop()
+        stacks[finalStack-1].append(crateToMove)
+        times += 1
+
+
+for eachLine in inputFileLines:
+    listOfLineSegments = eachLine.split(' ')
+    quantity = int(listOfLineSegments[1])
+    startStack = int(listOfLineSegments[3])
+    finalStack = int(listOfLineSegments[5])
+
+    moveCrates(quantity, startStack, finalStack)
+
+
+finalTopCrates = ''
+for stack in stacks:
+    finalTopCrates += str(stack[len(stack) - 1])
+
+print(finalTopCrates)
