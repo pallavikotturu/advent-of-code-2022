21a42418c118b0712d384c1ae0ef50a298029e26
jffhff <jffhff+github@thehoffmanns.net>
2023-01-10 22:20:40 -0500

    Day 6, Challenge 2

--- advent-day-6/datapackets.py
+++ advent-day-6/datapackets.py
@@ -1,32 +1,27 @@
 # for every substring [x,y] if any character in that substring is repeated, move to [x++, y++]
-inputFile = open('datapackets-input-pallavi.txt', 'r')
+inputFile = open('datapackets-input-jeff.txt', 'r')
 input = inputFile.readlines()[0]
 
 # created for use case of string length four
 def hasRepeatedCharacters(s) :
-    if s[0] == s[1]:
-        return True
-    if s[0] == s[2]:
-        return True
-    if s[0] == s[3]:
-        return True
-    if s[1] == s[2]:
-        return True
-    if s[1] == s[3]:
-        return True
-    if s[2] == s[3]:
-        return True
+    # common algorithm for comparing through all elements in list
+    for x in range(0, len(s)-1):
+        for y in range(x+1, len(s)):
+            if s[x] == s[y]:
+                return True
     return False
 
 
+startTokenSize = 14
 # for each index
 for i in range(len(input)-3):
     # substring = substring[startindex, startindex + 4]
-    substring = input[i:i+4]
+    substring = input[i:i+startTokenSize]
     # if any characters repeat
     if(hasRepeatedCharacters(substring)):
         #   call itself recursively
         pass
     else:
-        print(i+4)
+        print(i+startTokenSize)
+        print(substring)
         break
