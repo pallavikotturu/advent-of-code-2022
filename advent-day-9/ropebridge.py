33ecbf603fe6fc60756e44f0be1d3769aba6bc04
pkotturu3 <pkotturu@gatech.edu>
2023-03-08 22:23:30 -0500

    Day 9 finished everything.

--- advent-day-9/ropebridge.py
+++ advent-day-9/ropebridge.py
@@ -1,34 +1,51 @@
-# SMALL FUNCTIONS:
-
-# read command
-# move head based on command
-# calculate delta between head and tail (condition maybe?)
-# generate acceptable coordinates for tail to be in
-# if tail isn't in acceptable coordinate, move tail left, right, down, up
-# record every position that tail has visited
-
-# QUESTIONS TO ANSWER:
-# what datastructure to use for representing head and tail?
 head = [0, 0]
 tail = [0, 0]
 record_of_tail = [[0, 0]]
 
-# how to capture positions that tail has visited?
+# constants to set where the x and y values are in our array of coordinates
+X = 0
+Y = 1
 
-sample_instructions = """ R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2 """
+# for line in open('directions-pallavi.txt', 'r').readlines():
+for line in open('directions-jeff.txt', 'r').readlines():
+    (direction, number) = line.split(' ')
+    for i in range(int(number)):
+        if direction == 'R':
+            head[X] = head[X] + 1
+            if head[X] - tail[X] == 2:
+                tail[X] = tail[X] + 1
+                tail[Y] = head[Y]
 
-# reads commands
-# record tail position
-# reads commands & moves head
-# moves tail u
-# moves tail d
-# moves tail r
-# moves tail l
-# reads commands & moves head & move tail & record tail position
+        if direction == 'L':
+            head[X] = head[X] - 1
+            if tail[X] - head[X] == 2:
+                tail[X] = tail[X] - 1
+                tail[Y] = head[Y]
+
+        if direction == 'D':
+            head[Y] = head[Y] - 1
+            if tail[Y] - head[Y] == 2:
+                tail[Y] = tail[Y] - 1
+                tail[X] = head[X]
+
+        if direction == 'U':
+            head[Y] = head[Y] + 1
+            if head[Y] - tail[Y] == 2:
+                tail[Y] = tail[Y] + 1
+                tail[X] = head[X]
+
+
+    # check if the record has the current tail
+        match = False
+        for point in record_of_tail:
+            if point[X]==tail[X] and point[Y]==tail[Y]:
+                match = True
+
+        # if it doesn't, then add tail to the list
+        if not match:
+            record_of_tail.append([tail[X], tail[Y]])
+
+print(record_of_tail)
+print(len(record_of_tail))
+
+
