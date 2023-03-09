4e36409b4b321e86124080555630406dceaec637
pkotturu3 <pkotturu@gatech.edu>
2023-03-08 22:23:30 -0500

    Day 9 head finished and recorded

--- advent-day-9/ropebridge.py
+++ advent-day-9/ropebridge.py
@@ -14,19 +14,37 @@ tail = [0, 0]
 record_of_tail = [[0, 0]]
 
 # how to capture positions that tail has visited?
-
-sample_instructions = """ R 4
+sample_instructions = """R 4
 U 4
 L 3
 D 1
 R 4
 D 1
 L 5
-R 2 """
+R 2"""
 
-# reads commands
+X = 0
+Y = 1
+
+# reads command
 # record tail position
 # reads commands & moves head
+for line in sample_instructions.split("\n"):
+    (direction, number) = line.split(' ')
+    for i in range(int(number)):
+        if direction == 'R':
+            head[X] = head[X] + 1
+        if direction == 'L':
+            head[X] = head[X] - 1
+        if direction == 'D':
+            head[Y] = head[Y] + 1
+        if direction == 'U':
+            head[Y] = head[Y] - 1
+        record_of_tail.append([head[X], head[Y]])
+
+
+print(record_of_tail)
+
 # moves tail u
 # moves tail d
 # moves tail r
