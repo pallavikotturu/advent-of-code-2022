a1f81807f6dd764c42a90a4b4f453ad0647edb44
jffhff <jffhff+github@thehoffmanns.net>
2023-03-29 20:56:38 -0400

    Partial day 9, part 2

--- advent-day-9/ropebridge.py
+++ advent-day-9/ropebridge.py
@@ -1,49 +1,85 @@
 head = [0, 0]
-tail = [0, 0]
+tail = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
 record_of_tail = [[0, 0]]
 
 # constants to set where the x and y values are in our array of coordinates
 X = 0
 Y = 1
 
+def moveHead(direction):
+    # move the head according to the command
+    if direction == 'R':
+        head[X] = head[X] + 1
+    if direction == 'L':
+        head[X] = head[X] - 1
+    if direction == 'D':
+        head[Y] = head[Y] - 1
+    if direction == 'U':
+        head[Y] = head[Y] + 1
+
+def moveTail(tail, head):
+    # Auto-move tail according to rules
+    if head[X] - tail[X] == 2 and head[Y] - tail[Y] == 2:
+        tail[X] = tail[X] + 1
+        tail[Y] = tail[Y] + 1
+        return
+    if head[X] + tail[X] == 2 and head[Y] + tail[Y] == 2:
+        tail[X] = tail[X] - 1
+        tail[Y] = tail[Y] - 1
+        return
+    if head[X] - tail[X] == 2 and head[Y] + tail[Y] == 2:
+        tail[X] = tail[X] + 1
+        tail[Y] = tail[Y] - 1
+        return
+    if head[X] + tail[X] == 2 and head[Y] - tail[Y] == 2:
+        tail[X] = tail[X] - 1
+        tail[Y] = tail[Y] + 1
+        return
+
+    if head[X] - tail[X] == 2:
+        tail[X] = tail[X] + 1
+        tail[Y] = head[Y]
+        return
+    if tail[X] - head[X] == 2:
+        tail[X] = tail[X] - 1
+        tail[Y] = head[Y]
+        return
+    if tail[Y] - head[Y] == 2:
+        tail[Y] = tail[Y] - 1
+        tail[X] = head[X]
+        return
+    if head[Y] - tail[Y] == 2:
+        tail[Y] = tail[Y] + 1
+        tail[X] = head[X]
+        return
+
 # for line in open('directions-pallavi.txt', 'r').readlines():
 for line in open('directions-jeff.txt', 'r').readlines():
+#for line in ["R 5", "U 8", "L 8", "D 3", "R 17", "D 10", "L 25", "U 20"]:
     (direction, number) = line.split(' ')
     for i in range(int(number)):
-        if direction == 'R':
-            head[X] = head[X] + 1
-            if head[X] - tail[X] == 2:
-                tail[X] = tail[X] + 1
-                tail[Y] = head[Y]
+        moveHead(direction)
+        moveTail(tail[0], head)
+        moveTail(tail[1], tail[0])
+        moveTail(tail[2], tail[1])
+        moveTail(tail[3], tail[2])
+        moveTail(tail[4], tail[3])
+        moveTail(tail[5], tail[4])
+        moveTail(tail[6], tail[5])
+        moveTail(tail[7], tail[6])
+        moveTail(tail[8], tail[7])
 
-        if direction == 'L':
-            head[X] = head[X] - 1
-            if tail[X] - head[X] == 2:
-                tail[X] = tail[X] - 1
-                tail[Y] = head[Y]
-
-        if direction == 'D':
-            head[Y] = head[Y] - 1
-            if tail[Y] - head[Y] == 2:
-                tail[Y] = tail[Y] - 1
-                tail[X] = head[X]
-
-        if direction == 'U':
-            head[Y] = head[Y] + 1
-            if head[Y] - tail[Y] == 2:
-                tail[Y] = tail[Y] + 1
-                tail[X] = head[X]
 
 
     # check if the record has the current tail
         match = False
         for point in record_of_tail:
-            if point[X]==tail[X] and point[Y]==tail[Y]:
+            if point[X]==tail[0][X] and point[Y]==tail[0][Y]:
                 match = True
 
         # if it doesn't, then add tail to the list
         if not match:
-            record_of_tail.append([tail[X], tail[Y]])
+            record_of_tail.append([tail[0][X], tail[0][Y]])
 
 print(record_of_tail)
 print(len(record_of_tail))
